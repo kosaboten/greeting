7850b32c0e1c77fa371482f08c193c1cedb81ea1
kosaboten <saboten.system@gmail.com>
2023-10-22 18:22:25 +0900

    単純な挨拶を実装

---
+++ app/Http/Controllers/GreetingController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GreetingController extends Controller
+{
+    public function greeting($time_frame)
+    {
+        switch ($time_frame){
+            case 'morning':
+                $time = '朝のあいさつ';
+                $msg = 'おはようございます';
+                break;
+            case 'afternoon':
+                $time = '昼のあいさつ';
+                $msg = 'こんにちは';
+                break;
+            case 'evening':
+                $time = '夕方のあいさつ';
+                $msg = 'こんばんは';
+                break;
+            case 'night':
+                $time = '夜のあいさつ';
+                $msg = 'おやすみ';
+                break;
+            default:
+                $time = '時間帯を指定してください';
+                $msg = 'ごきげんよう';
+                break;
+        }
+        return view('greeting.timeframe', ['time' => $time, 'msg' =>$msg]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GreetingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comments/{timeframe}', [GreetingController::class, 'greeting']);
